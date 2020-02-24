d63403320a75acdfb1530a18f35d348ed844cb8a
Christian Chaumette-Brown <cchaumette-brown@ucmerced.edu>
2020-02-24 10:18:55 -0800

    Initial Commit

---
+++ Authenticator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Authenticator
+//
+//  Created by Christian Chaumette-Brown on 2/24/20.
+//  Copyright © 2020 Christian Chaumette-Brown. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
