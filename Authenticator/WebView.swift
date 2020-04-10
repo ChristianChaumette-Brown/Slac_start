4ab34f9bbaa9eee73bde16f8a7c74375c9f497dc
Christian Chaumette-Brown <cchaumette-brown@ucmerced.edu>
2020-04-10 02:27:53 -0700

    beginning of website masking

--- Authenticator/WebView.swift
+++ Authenticator/WebView.swift
@@ -11,10 +11,11 @@ import WebKit
 
 class WebView: UIViewController, WKNavigationDelegate {
     @IBOutlet weak var username:UITextField!
-    
+   // var timer = Timer()
     var webView: WKWebView!
     //let webView = WKWebView()
    var counter = 0
+    var vertOffs = 0
     lazy var loginButton: UIButton = {
         let button = UIButton()
         button.setTitle("Login", for: .normal)
@@ -27,20 +28,21 @@ class WebView: UIViewController, WKNavigationDelegate {
     }()
     lazy var myLogin: UITextField = {
           let text = UITextField()
-          text.frame = CGRect(x: 0, y: 0, width: 300.00, height: 30.00);
+          text.frame = CGRect(x: 0, y: 0, width: 300.00, height: 5.00);
           text.text=""
           text.placeholder="Username"
-          text.borderStyle=UITextField.BorderStyle.bezel
-          text.backgroundColor=UIColor.blue
+          //text.borderStyle=UITextField.BorderStyle.roundedRect
+          text.backgroundColor=UIColor.clear
           text.textColor=UIColor.black
+        text.layer.borderColor = UIColor.clear.cgColor
           text.translatesAutoresizingMaskIntoConstraints = false
           text.layer.cornerRadius = 25
-          text.textAlignment = .center
+          text.textAlignment = .left
           return text
       }()
     lazy var myPass: UITextField = {
              let text = UITextField()
-             text.frame = CGRect(x: 0, y: 0, width: 300.00, height: 30.00);
+             text.frame = CGRect(x: 0, y: 0, width: 300.00, height: 20.00);
              text.text=""
              text.placeholder="Password"
              text.borderStyle=UITextField.BorderStyle.bezel
@@ -57,15 +59,22 @@ class WebView: UIViewController, WKNavigationDelegate {
         
          webView.navigationDelegate = self
         view=webView
+        
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+       // timer = Timer.scheduledTimer(timeInterval: 3.0, target: self, selector: #selector(timerAction), userInfo: nil, repeats: true)
         //let url=URL(string: "https://www6.slac.stanford.edu/slac-staff")!
         let url = URL(string: "https://intranet.slac.stanford.edu/")!
         //webView.frame = CGRect(x: 0, y: 200, width: 100, height: 200)
+       // view.frame.size
+        webView.sizeThatFits(view.frame.size)
         webView.load(URLRequest(url: url))
         webView.allowsBackForwardNavigationGestures = true
+        webView.scrollView.isScrollEnabled = false
+        webView.scrollView.bounces = false
+        webView.scrollView.canCancelContentTouches = true
         
         view.addSubview(loginButton)
         view.addSubview(myLogin)
@@ -75,21 +84,35 @@ class WebView: UIViewController, WKNavigationDelegate {
         loginButton.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         loginButton.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: 200).isActive = true
         
-        myLogin.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: 110).isActive = true
-        myLogin.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        myLogin.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -80).isActive = true
+        myLogin.centerXAnchor.constraint(equalTo: view.centerXAnchor, constant: 138).isActive = true
         myLogin.widthAnchor.constraint(equalToConstant: 300).isActive = true
         
         myPass.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: 150).isActive = true
         myPass.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         myPass.widthAnchor.constraint(equalToConstant: 300).isActive = true
     }
-    
+    /*
+    @objc func timerAction(){
+       print("timer fired!")
+        webView.evaluateJavaScript("document.getElementsByName('username')[0].offsetTop;", completionHandler: {(value,error) in
+        
+        print("Vertical offset value \(value)")
+        print("Error grabbing offset \(error)")
+            self.vertOffs = value! as! Int
+            self.myLogin.centerYAnchor.constraint(equalTo: self.view.centerYAnchor, constant: CGFloat(self.vertOffs)).isActive = true
+            //self.loadView()
+           // self.view.reloadInputViews()
+    }
+        )}
+    */
     
     @IBAction func onButtonPressed(_ sender: UIButton){
        // switch counter{
         //case 0:
             let output = myLogin.text!
             var output1 = myPass.text!
+        var vertOffset = 0
             webView.evaluateJavaScript("document.getElementsByName('username')[0].value='\(output)'", completionHandler: {(value,error) in
           // webView.evaluateJavaScript("document.getElementsByTagName('html')[0].innerHTML", completionHandler: {(value,error) in
                 print("Inner HTML: \(value) \(output)")
@@ -104,12 +127,19 @@ class WebView: UIViewController, WKNavigationDelegate {
                 output1 = ""
                 self.myPass.text = ""
             })
-            userID = self.myLogin.text!
+            userID = output
        // case 1:
             webView.evaluateJavaScript("document.getElementsByName('login')[0].submit();", completionHandler: {(value,error) in
                 print("Value \(value)")
                 
                 print("Error \(error)")
+           
+                self.webView.evaluateJavaScript("document.getElementsByName('username')[0].offsetTop;", completionHandler: {(value,error) in
+                    
+                    print("Vertical offset value \(value)")
+                    print("Error grabbing offset \(error)")
+                    vertOffset = value! as! Int
+                })
             })
       //  case 2: break
         //case 3: break
@@ -117,7 +147,7 @@ class WebView: UIViewController, WKNavigationDelegate {
        // case 5: break
        // default:break
             
-            
+        myLogin.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: CGFloat(vertOffset+8)).isActive = true
             
         //}
         
